8a278c193c85d2931e3317b6ef7ef5c96945c121
Manuel Vonthron <manuel.vonthron@acadis.org>
2014-12-17 23:49:14 -0500

    parsing XML file
    (much slower but readable at least)

--- import.py
+++ import.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
 import os
-import datetime
+from datetime import datetime, timedelta
+
 from collections import defaultdict
 import rrdtool
 import re
 
-MUNIN_FOLDER = "data/acadis.org"
+import xml.etree.ElementTree as ET
+from scipy.special._ufuncs import nbdtr
 
+MUNIN_FOLDER = "data/acadis.org"
+KEEP_AVERAGE_ONLY = True
 
 # Munin types
 DATA_TYPES = {
@@ -53,10 +57,20 @@ def readRRDFile(filename):
     lastupdate = datetime.datetime.fromtimestamp(int(infos['last_update']))
     step = int(infos["step"])
     print lastupdate
+    print infos
+    key0 = elements[0][0:elements[0].index('[')]
+    key1 = elements[1][0:elements[1].index('[')]
 
-    rra_index = set()
-    ds_index = set()
-    for line in infos:
+    print key0, key1
+
+    if key0 == "rra":
+        rras[idx0][key1][idx1] = value
+
+        if len(elements) > 2:
+            print "That's new"
+        else:
+            item[line] = value
+
         if line.startswith("rra"):
             match = re.findall(r'\d+', line)
             if len(match) > 0:
@@ -66,17 +80,65 @@ def readRRDFile(filename):
             if len(match) > 0:
                 ds_index.add(match[0])
 
+        print item
+
+    print rras
     print rra_index, ds_index
-    for i in range(rra_index):
-        rra = defaultdict(dict)
+    # for i in range(rra_index):
+    #     rra = defaultdict(dict)
 
 
+def readRDDXML(filename):
+    print "Parsing XML file {0}".format(filename)
+    values = defaultdict(dict)
+
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    last_update = int(root.find('lastupdate').text)
+    step = int(root.find('step').text)
+
+    for ds in root.findall('ds'):
+        pass
+        # print ds.find('name').text.strip()
+        # print ds.find('type').text.strip()
+        #
+        # print ds.find('minimal_heartbeat').text.strip()
+        # print ds.find('last_ds').text.strip()
+
+    for rra in root.findall('rra'):
+        if KEEP_AVERAGE_ONLY and rra.find('cf').text.strip() != "AVERAGE":
+            # @todo store max and min in the same record but different column
+            continue
+
+        pdp_per_row = int(rra.find('pdp_per_row').text)
+        entry_delta = pdp_per_row*step
+        cf = rra.find('cf').text
+        last_entry = last_update - last_update % (entry_delta)
+        nb_entries = len(rra.find("database"))
+        entry_date = first_entry = last_entry - (nb_entries-1)*entry_delta
+
+        print "  + New segment from {0} to {1}. Granularity: {2} sec.".format(datetime.fromtimestamp(first_entry), datetime.fromtimestamp(last_entry), entry_delta)
+
+        # there should be only onv <v> entry per row, at least didn't see other cases with Munin
+        for v in rra.findall("./database/row/v"):
+            try:
+                value = float(v.text)
+            except:
+                value = None
+
+            values[entry_date] = value
+            entry_date += entry_delta
+
+    return values
 
 
 def main():
-    guessPluginListInFolder(MUNIN_FOLDER)
+    #guessPluginListInFolder(MUNIN_FOLDER)
 
-    readRRDFile(os.path.join(MUNIN_FOLDER, "house-youtube_subscribed_scilabus-count-g.rrd"))
+    #readRRDFile(os.path.join(MUNIN_FOLDER, "house-youtube_subscribed_scilabus-count-g.rrd"))
+
+    combined_values = readRDDXML(os.path.join(MUNIN_FOLDER, "xml", "house-youtube_subscribed_scilabus-count-g.rrd.xml"))
 
 if __name__ == "__main__":
     main()
