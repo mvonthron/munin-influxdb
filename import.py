053adc4f6a54b97ec15a01ab0fae5d8b9d7d2fdc
Manuel Vonthron <manuel.vonthron@acadis.org>
2014-12-19 00:13:19 -0500

    wip

--- import.py
+++ import.py
@@ -2,13 +2,18 @@
 
 import os
 from datetime import datetime, timedelta
+from influxdb import InfluxDBClient
+
 
 from collections import defaultdict
 import rrdtool
 import re
+import math
+import subprocess
+
+import cProfile
 
 import xml.etree.ElementTree as ET
-from scipy.special._ufuncs import nbdtr
 
 MUNIN_FOLDER = "data/acadis.org"
 KEEP_AVERAGE_ONLY = True
@@ -113,21 +118,35 @@ def readRDDXML(filename):
 
         pdp_per_row = int(rra.find('pdp_per_row').text)
         entry_delta = pdp_per_row*step
-        cf = rra.find('cf').text
-        last_entry = last_update - last_update % (entry_delta)
+        last_entry = last_update - last_update % entry_delta
         nb_entries = len(rra.find("database"))
         entry_date = first_entry = last_entry - (nb_entries-1)*entry_delta
 
-        print "  + New segment from {0} to {1}. Granularity: {2} sec.".format(datetime.fromtimestamp(first_entry), datetime.fromtimestamp(last_entry), entry_delta)
+        print "  + New segment from {0} to {1}. Nb entries: {2}. Granularity: {3} sec.".format(datetime.fromtimestamp(first_entry),
+                                                                                               datetime.fromtimestamp(last_entry),
+                                                                                               nb_entries,
+                                                                                               entry_delta)
 
         # there should be only onv <v> entry per row, at least didn't see other cases with Munin
         for v in rra.findall("./database/row/v"):
             try:
                 value = float(v.text)
+                # we don't want to override existing values as they are 'fresher' and less likely to be averaged (CF'd)
+                if math.isnan(value):
+                    pass
+                    #print entry_date, value
+                elif not entry_date in values:
+                    values[entry_date] = value
+
             except:
                 value = None
 
-            values[entry_date] = value
+            # if entry_date in values and values[entry_date] != value:
+            #     #
+            #     print " * {0} already has value {1}, new is {2} (probably averaged thus less precise)".format(datetime.fromtimestamp(entry_date),
+            #                                                                                                   values[entry_date],
+            #                                                                                                   value)
+
             entry_date += entry_delta
 
     return values
@@ -138,7 +157,46 @@ def main():
 
     #readRRDFile(os.path.join(MUNIN_FOLDER, "house-youtube_subscribed_scilabus-count-g.rrd"))
 
+    # cProfile.run("readRDDXML(os.path.join(MUNIN_FOLDER, 'xml', 'house-youtube_subscribed_scilabus-count-g.rrd.xml'))")
     combined_values = readRDDXML(os.path.join(MUNIN_FOLDER, "xml", "house-youtube_subscribed_scilabus-count-g.rrd.xml"))
+    print "Got {0} entries total".format(len(combined_values))
+
+    body = [{
+        'name': 'test',
+        'columns': ['time', 'value'],
+        'points': [ [x[0], x[1]] for x in combined_values.items()]
+    }]
+
+    client = InfluxDBClient('', 42, '', '', '')
+    client.write_points(body)
+
+def retrieveMuninConfiguration():
+    """
+    """
+    config = defaultdict(dict)
+    #read munin.conf to determine hostnames
+    with open("data/munin-conf/munin.conf") as f:
+        for line in f.readlines():
+            if line.startswith('['):
+                nodename = line.strip("[]\n").split(';')
+                if len(nodename) == 1:
+                   config[nodename] = {}
+                elif len(nodename) == 2:
+                    config[nodename[0]][nodename[1]] = {}
+                else:
+                    print "Doesn't sound right"
+    
+    #run "munin-run * config" to get list of plugins and config
+    plugins = os.listdir("data/munin-conf/plugins")
+    print plugins
+    
+    #for each host, find the /var/lib/munin/<host> directory and check 
+    #if node name and plugin conf match RRD files
+    for host in config:
+        plugins = os.listdir("data/{0}".format(host))
+    
+    print config
 
 if __name__ == "__main__":
-    main()
+    #main()
+    retrieveMuninConfiguration()
