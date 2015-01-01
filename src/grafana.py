379ec680c645c94b359dfb59b58081f606b8d3d4
Manuel Vonthron <manuel.vonthron@acadis.org>
2015-01-01 16:23:14 -0500

    added overrides on graph types

--- src/grafana.py
+++ src/grafana.py
@@ -32,7 +32,7 @@ class Panel:
         self.fill = 0
         self.stack = False
         self.leftYAxisLabel = None
-
+        self.overrides = []
         self.width = 6
 
     def add_query(self, column):
@@ -43,6 +43,35 @@ class Panel:
     def sort_queries(self, order):
         ordered_keys = order.split()
         self.queries.sort(key=lambda x: ordered_keys.index(x.column) if x.column in ordered_keys else len(self.queries))
+
+    def process_graph_type(self, plugin):
+        """
+        Munin processes draw types on a per metric basis whereas Grafana sets the type for
+        the whole panel. However overrides are possible since https://github.com/grafana/grafana/issues/425
+        http://munin-monitoring.org/wiki/fieldname.draw
+        """
+        draw_list = [(field, plugin.fields[field].settings.get("draw", "LINE2")) for field in plugin.fields]
+        hasStack = bool([x for x, y in draw_list if "STACK" in y])
+        hasArea = bool([x for x, y in draw_list if "AREA" in y])
+
+        if hasArea:
+            self.fill = 5
+        if hasArea:
+            self.stack = True
+
+        # build overrides list
+        self.overrides = []
+        for field, draw in draw_list:
+            current = {"alias": field}
+            # LINE* should be matched
+            if hasArea and draw.startswith("LINE"):
+                current["fill"] = 0
+            # LINE* should be matched *but not* LINESTACK*
+            if hasStack and draw.startswith("LINE") and not draw.startswith("LINESTACK"):
+                current["stack"] = False
+
+            if len(current) > 1:
+                self.overrides.append(current)
 
     def to_json(self, settings):
         return {
@@ -68,6 +97,7 @@ class Panel:
                 "alignAsTable": settings.grafana.show_minmax,
                 "rightSide": False
             },
+            "seriesOverrides": self.overrides,
             "leftYAxisLabel": self.leftYAxisLabel,
         }
 
@@ -75,6 +105,7 @@ class HeaderPanel(Panel):
     def __init__(self, title):
         self.title = title
         self.content = ""
+        self.series = None
 
     def to_json(self, _):
         return {
@@ -214,6 +245,8 @@ necessary:
                                 panel.fill = 5
                             if draw == "STACK":
                                 panel.stack = True
+                            if draw.startswith("LINE"):
+                                pass
 
                         i += 1
                         progress_bar(i, self.settings.nb_rrd_files)
@@ -227,6 +260,7 @@ necessary:
                     if "graph_order" in _plugin.settings:
                         panel.sort_queries(_plugin.settings["graph_order"])
 
+                    panel.process_graph_type(_plugin)
 
 if __name__ == "__main__":
     # main for dev/debug purpose only
